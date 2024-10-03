a35b0abf9fc142068e612cbeae585d9116b18d77
Ahmad Fauzidan <zidankhainur2@gmail.com>
2024-10-04 01:50:01 +0700

    add links for surat

--- app/components/PopUpPesan.tsx
+++ app/components/PopUpPesan.tsx
@@ -10,10 +10,11 @@ export default function PopupPesan() {
         </p>
 
         <h2 className="text-white mt-6 text-2xl font-semibold">
-          Hallo Tenshi-chan {"(⁠≧⁠▽⁠≦⁠)"}, hbd yhhh. ini aku ada surat buat kamu. tolong dibaca yaaa!
+          Hallo Tenshi-chan {"(⁠≧⁠▽⁠≦⁠)"}, hbd yhhh. ini aku ada surat buat
+          kamu. tolong dibaca yaaa!
         </h2>
 
-        <a href="#">
+        <a href="https://wphcaxizypgvkbhwkqss.supabase.co/storage/v1/object/public/user%20image/surat/HAPPY%20BIRTHDAY%20ALYA.pdf?t=2024-10-03T18%3A46%3A48.965Z">
           <Button className="font-medium text-lg sm:text-md hover:bg-red-700 mt-6">
             <Download className="mr-2 h-4 w-4" /> Download
           </Button>
